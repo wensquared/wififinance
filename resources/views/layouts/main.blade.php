d134f469216872070a292e03a933435660ceb732
wensquared <71566436+wensquared@users.noreply.github.com>
2021-10-30 16:41:47 +0200

    #5 add register and login blade

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('pageTitle',config('app.name'))</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header class="p-3 bg-dark text-white">
+
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                    <!-- <li><a href="#" class="nav-link px-2 text-white">Features</a></li> -->
+                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                    <!-- <li><a href="#" class="nav-link px-2 text-white">About</a></li> -->
+                </ul>
+        
+                <div class="text-end">
+                    @guest
+                        @if (Route::has('login'))
+                            <a href="{{ route('login')}}"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register')}}"><button type="button" class="btn btn-warning">Sign-up</button></a>
+                        @endif
+                    @else
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->username }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    @endguest
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+
+    </main>
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+            <p>&copy; 2021 WIFI Finance, Inc. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
