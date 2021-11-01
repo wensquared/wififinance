eb98430407eb99086a9f64666e8b5d6cc3007f38
wensquared <71566436+wensquared@users.noreply.github.com>
2021-11-01 18:24:35 +0100

    disabled forgot password link in login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link disabled" href="{{ route('password.request') }}" >
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
